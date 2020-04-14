312310773499ed8bed8f3958d3eef7e0650b5f2c
Mrenoir <44065639+Mrenoir@users.noreply.github.com>
2020-04-15 01:20:09 +0200

    Update classification.rs
    correction

--- Project/Lib/Rosenblatt/src/classification.rs
+++ Project/Lib/Rosenblatt/src/classification.rs
@@ -8,17 +8,19 @@ fn predict_linear_classification(w:Vec<f32>, xk: &Vec<f32>)-> i8{
     }
     return if sum >= 0.0 { 1 } else { -1 }
 }
-fn train_rosenblatt(mut w: Vec<Vec<f32>>, x: Vec<Vec<f32>>, y:&mut[f32], nb_iter:&mut i32, alpha : &mut i32){
-   let i : i16 = 0;
-    for it in 0 .. nb_iter  {
-       let k:i16 = 0; // Random à mettre
+
+fn train_rosenblatt(mut w: Vec<Vec<f32>>, x: Vec<Vec<f32>>, y:&mut[f32], nb_iter:&mut i32, alpha : &mut i32) {
+    let i: i16 = 0;
+    for it in 0..nb_iter {
+        let k: i16 = 0; // Random à mettre
         let gxk: i32 = 0;
         //gxk = predict_linear_classification(w,x[k]);
-        for i  in 0.. x[1].len()  {
-            w[i+1] += alpha * (y[k] - gxk ) * x[k][i];
+        for i in 0..x[1].len() {
+            w[i + 1] += alpha * (y[k] - gxk) * x[k][i];
         }
         w[0] += alpha * (y[k] - gxk);
     }
+}
 
 fn main(){
     let w = vec![0.9, 0.8, 0.36];
