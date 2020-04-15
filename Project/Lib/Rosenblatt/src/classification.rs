6885e8c0978cfda1e259f08f8d43aba6166b6b30
Quentin <quentin.clodion@gmail.com>
2020-04-15 12:05:26 +0200

    correction

--- Project/Lib/Rosenblatt/src/classification.rs
+++ Project/Lib/Rosenblatt/src/classification.rs
@@ -1,7 +1,7 @@
 extern crate rand;
 use rand::Rng;
 
-fn predict_linear_classification(w:Vec<f32>, xk: &Vec<f32>)-> i8{
+fn predict_linear_classification(w:&Vec<f32>, xk:&Vec<f32>)-> i8{
     let mut sum = w[0];
     for i in 0..xk.len(){
         sum += w[i + 1] * xk[i];
@@ -9,25 +9,24 @@ fn predict_linear_classification(w:Vec<f32>, xk: &Vec<f32>)-> i8{
     return if sum >= 0.0 { 1 } else { -1 }
 }
 
-fn train_rosenblatt(mut w: Vec<Vec<f32>>, x: Vec<Vec<f32>>, y:Vec<i8>, nb_iter:i32, alpha :f32) {
-    for it in 0..nb_iter {
-       let  k = 0; // Random à mettre
-        let gxk: i32 = 0;
-        //gxk = predict_linear_classification(w,x[k]);
+fn train_rosenblatt(w:&mut Vec<f32>, x:&Vec<Vec<f32>>, y:&Vec<i8>, nb_iter:i32, alpha:f32) {
+    for _it in 0..nb_iter {
+        let k = 0;
+        let gxk = predict_linear_classification(w,&x[k]);
         for i in 0..x[1].len() {
             w[i + 1] += alpha * (y[k] - gxk as i8) as f32 * x[k][i];
         }
-        w[0] +=   alpha * (y[k] - gxk as i8) as f32;
+        w[0] += alpha * (y[k] - gxk as i8) as f32;
     }
 }
 
 fn main(){
-    let w = vec![0.9, 0.8, 0.36];
-    let y = vec![-1,1,1];
-    let xk = vec![vec![0.2, 0.4], vec![0.73, 0.94]];
-    let yk = vec![vec![0.2, 0.4], vec![0.73, 0.94]];
-    let alpha : f32 = 0.1;
-    let nb_it = 15;
-    train_rosenblatt(xk, yk, y, *&nb_it, *&alpha);
-    println!("{}",predict_linear_classification(w, &xk[0]));
+    let mut w = vec![0.9, 0.8, 0.36];
+    let x = vec![vec![0.2, 0.4], vec![0.73, 0.94]];
+    let y = vec![-1,1];
+    
+    println!("{:?}",w);
+    train_rosenblatt(&mut w, &x, &y, 15, 0.1);
+    println!("{:?}",w);
 }
+
