4ad785806929fd019a79c05d6be9a9c2bb7c1600
Quentin <quentin.clodion@gmail.com>
2020-04-22 12:08:30 +0200

    linear regression

--- Project/Lib/Rosenblatt/src/classification.rs
+++ Project/Lib/Rosenblatt/src/classification.rs
@@ -1,6 +1,7 @@
 extern crate rand;
-extern crate ndarray;
+extern crate nalgebra;
 use rand::Rng;
+use nalgebra::DMatrix;
 
 fn generate_rand_f32vector(size: i16, start: f32, end: f32) -> Vec<f32>{
     let mut vector: Vec<f32> = vec![];
@@ -32,25 +33,26 @@ fn train_rosenblatt(w:&mut Vec<f32>, x:&Vec<Vec<f32>>, y:&Vec<i8>, nb_iter:i32, 
 
 fn main(){
     //classification
-    let mut w = generate_rand_f32vector(3, 0.0, 1.0);
+   /* let mut w = generate_rand_f32vector(3, 0.0, 1.0);
     let x = vec![vec![0.2, 0.4], vec![0.73, 0.94]];
     let y = vec![-1,1];
     
     println!("{:?}",w);
     train_rosenblatt(&mut w, &x, &y, 15, 0.1);
     println!("{:?}",w);
-
+*/
     //regression
-    /*
-    let mut x: Vec<f32> = vec![];
-    let mut y: Vec<f32> = vec![];
+    let mut x = [0f32;40];
+    let mut y = [0f32;20]; 
     let mut rng = rand::thread_rng();
     for i in 0..20{
-        x.push(i as f32);
-        y.push(1.5 * (i as f32) + 0.75 + ((rng.gen_range(0.0,1.0) - 0.35) * 2.0));
+        x[i * 2] = 1.0;
+        x[i * 2 + 1]  = (i + 1) as f32;  
+        y[i] = 1.5 * ((i + 1) as f32) + 0.75 + ((rng.gen_range(0.0,1.0) - 0.35) * 2.0);
     }
-    println!("{:?}",x);
-    println!("{:?}",y);
-*/
+    let xm = DMatrix::from_row_slice(20,2,&x);
+    let ym = DMatrix::from_row_slice(20,1,&y);
+    let w = (((xm.transpose() * &xm).try_inverse()).unwrap() * xm.transpose()) * ym;
+    println!("{:?}",w);
 }
 
