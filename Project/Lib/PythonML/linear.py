bd82d78d05016d52f2a9450fea562513ec2f183f
Quentin Clodion <losabit@Noe.localdomain>
2020-06-11 16:00:19 +0000

    start python lib

---
+++ Project/Lib/PythonML/linear.py
@@ -0,0 +1,82 @@
+import ctypes
+
+
+class Linear:
+    def __init__(self, dll_path):
+        self.lib = ctypes.CDLL(dll_path)
+        self.initialize_rust_functions()
+        self.model = None
+        self.model_size = 0
+
+    def initialize_rust_functions(self):
+        self.lib.init_linear_model.argtypes = [ctypes.c_int]
+        self.lib.init_linear_model.restype = ctypes.c_void_p
+
+        self.lib.predict_linear_model_regression.restype = ctypes.c_double
+        self.lib.predict_linear_model_regression.argtypes = [
+            ctypes.c_void_p,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int
+        ]
+
+        self.lib.predict_linear_model_classification.restype = ctypes.c_double
+        self.lib.predict_linear_model_classification.argtypes = [
+            ctypes.c_void_p,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int
+        ]
+
+        self.lib.train_linear_model_classification.restype = None
+        self.lib.train_linear_model_classification.argtypes = [
+            ctypes.c_void_p,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_double
+        ]
+
+        self.lib.train_linear_model_regression.restype = ctypes.POINTER(ctypes.c_double)
+        self.lib.train_linear_model_regression.argtypes = [
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int
+        ]
+
+    def init_linear_model(size):
+        self.model = self.lib.init_linear_model(ctypes.c_int(size))
+        self.model_size = size + 1
+
+    def predict_linear_model_classification(points):
+        return self.lib.predict_linear_model_classification(
+            self.model,
+            points.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            len(points)
+        )
+    
+     def predict_linear_model_regression(points):
+        return self.lib.predict_linear_model_regression(
+            self.model,
+            points.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            len(points)
+        )
+
+    def train_linear_model_classification(x, y, result_size, nb_iter, alpha):
+        self.lib.train_linear_model_classification(
+            self.model,
+            x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            y.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            self.size - 1,
+            result_size,
+            nb_iter,
+            alpha
+        )
+    
+    def train_linear_model_regression(x, y, x_size):
+        return self.lib.train_linear_model_regression(
+            x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            y.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            x_size
+        )
+
