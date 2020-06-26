971695ee5adfb5e2a6cc4cba6121c638d9abf173
Quentin Clodion <losabit@Noe.localdomain>
2020-06-26 19:48:15 +0000

    Some corrections for python implementation of MLP

--- Project/Lib/PythonML/multicouche.py
+++ Project/Lib/PythonML/multicouche.py
@@ -6,12 +6,10 @@ class MLP:
     def __init__(self, dll_path):
         self.lib = ctypes.CDLL(dll_path)
         self.initialize_rust_functions()
-        self.modelout = None
-        self.out = []
         self.model = []
-        self.nbCouche = 0
+        self.neurones_by_couche = []
 
-    def initialize_rust_functions(self, nbNeuronneCouche):
+    def initialize_rust_functions(self):
         self.lib.init_multicouche_model.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
         self.lib.init_multicouche_model.restype = ctypes.POINTER(ctypes.c_double)
 
@@ -50,37 +48,45 @@ class MLP:
             ctypes.c_int,
             ctypes.c_double]
         self.lib.train_multicouche_model_regression.restype = ctypes.POINTER(ctypes.c_double)
-        self.nbCouche = nbNeuronneCouche
 
     def init_multicouche_model(self, neurones_by_couche):
+        self.neurones_by_couche = neurones_by_couche
         self.model = self.lib.init_multicouche_model(
             neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
             ctypes.c_int(len(neurones_by_couche)))
 
     def predict_multicouche_model_classification(self, x):
-        return self.lib.predict_multicouche_model_classification(self.model, x, self.nbNeuronneCouche, self.nbCouche)
+        return self.lib.predict_multicouche_model_classification(
+            self.model, 
+            x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), 
+            self.neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)), 
+            len(self.neurones_by_couche))
 
     def predict_multicouche_model_regression(self, x):
-        return self.lib.predict_multicouche_model_regression(self.model, x, self.nbNeuronneCouche, self.nbCouche)
+        return self.lib.predict_multicouche_model_regression(
+            self.model, 
+            x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), 
+            self.neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)), 
+            len(self.neurones_by_couche))
 
-    def train_multicouche_model_classification(self, x, y, neurones_by_couche, nbExemple, nbIter, alpha):
-        return self.lib.train_multicouche_model_classification(
+    def train_multicouche_model_classification(self, x, y, nbExemple, nbIter, alpha):
+        self.model = self.lib.train_multicouche_model_classification(
             self.model,
             x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             y.ctypes.data_as(ctypes.POINTER(ctypes.c_int8)),
-            neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
-            self.nbCouche,
+            self.neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+            len(self.neurones_by_couche),
             nbExemple,
             nbIter,
             alpha)
 
-    def train_multicouche_model_regression(self, x, y, neurones_by_couches, nbExemple, nbIter, alpha):
-        return self.lib.train_multicouche_model_regression(
+    def train_multicouche_model_regression(self, x, y, nbExemple, nbIter, alpha):
+        self.model = self.lib.train_multicouche_model_regression(
             self.model,
             x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             y.ctypes.data_as(ctypes.POINTER(ctypes.c_int8)),
-            neurones_by_couches.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
-            self.nbCouche,
+            self.neurones_by_couche.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+            len(self.neurones_by_couche),
             nbExemple,
             nbIter,
             alpha)
