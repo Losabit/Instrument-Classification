7db642ea078386c2984511503b00d13fed057c95
Marvin <44065639+Mrenoir@users.noreply.github.com>
2020-07-07 10:56:47 +0200

    correction entete fonct

--- Project/Lib/SupervisingML/src/rbf.rs
+++ Project/Lib/SupervisingML/src/rbf.rs
@@ -4,12 +4,12 @@ use rand::seq::index::sample;
 
 struct RBF { w: DMatrix<f64> , x: DMatrix<f64> , gamma : f64 , }
 
-pub extern fn randvalue(size: usize, it : usize) -> f64 {
+pub extern "C" fn randvalue(size: usize, it : usize) -> f64 {
     let mut rng = rand::thread_rng();
     let myrand = (rng.gen_range(0,it) % size as f64 as usize) as f64;
     return myrand;
 }
-pub  extern  fn init_rbf(w : DMatrix<f64> , x :DMatrix<f64> , gamma: f64) -> RBF{
+pub  extern  "C" fn init_rbf(w : DMatrix<f64> , x :DMatrix<f64> , gamma: f64) -> RBF{
     let r = RBF{
         w,
         x,
@@ -17,7 +17,7 @@ pub  extern  fn init_rbf(w : DMatrix<f64> , x :DMatrix<f64> , gamma: f64) -> RBF
     };
     return r;
 }
-pub extern  fn rbf_naive_predict_classification() -> f64{
+pub extern "C" fn rbf_naive_predict_classification() -> f64{
     if(){
         1.0
     }
@@ -27,20 +27,20 @@ pub extern  fn rbf_naive_predict_classification() -> f64{
 
 }
 
-pub  extern fn train_native_rbf (x : Box<[f64]>, y : Box<[f64]>, input_per_sample: f64, nbSample : f64, gamma : f64 ) -> RBF{
-    let  Xm =  DMatrix::from_row_slice(input_per_sample,nbSample,&x);
-    let Ym = DMatrix::from_row_slice(input_per_sample,1,&y);
-    let phiMat = DMatrix::from_row_slice(input_per_sample,nbSample,0);
+pub  extern "C" fn train_native_rbf (x : * f64, y : * f64, input_per_sample: f64, nbSample : f64, gamma : f64 ) -> RBF{
+    let xm =  DMatrix::from_row_slice(input_per_sample, nbSample, &x);
+    let ym = DMatrix::from_row_slice(input_per_sample, 1, &y);
+    let phi_mat = DMatrix::from_row_slice(input_per_sample, nbSample, 0);
     unsafe {
         for i in nbSample {
             for j in input_per_sample {
-                let alpha = Xm.row(i) - Xm.row(j);
+                let alpha = xm.row(i) - xm.row(j);
                 let toexp = -gamma * (alpha.pow(2));
-                phiMat(i, j) = toexp.exp();
+                phi_mat(i, j) = toexp.exp();
             }
         }
     }
-    let Wm = phiMat.try_inverse() * Ym;
-   let rbf =  init_rbf(Wm,Xm,gamma);
+    let wm = phi_mat.try_inverse() * ym;
+   let rbf =  init_rbf(wm, xm, gamma);
     return rbf;
 }
